8d825807d9c44d226012a05ff95068b3b92e3cf1
Areyouthere003 <jgerazo7@misena.edu.co>
2023-05-18 13:34:37 -0500

    se agregó punto faltante

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -41,9 +41,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(59, 96);
+            this.label1.Location = new System.Drawing.Point(187, 273);
+            this.label1.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.Size = new System.Drawing.Size(249, 37);
             this.label1.TabIndex = 0;
             this.label1.Text = "Insertar Número";
             // 
@@ -53,25 +54,28 @@
             this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Blue;
-            this.label2.Location = new System.Drawing.Point(242, 32);
+            this.label2.Location = new System.Drawing.Point(766, 91);
+            this.label2.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(346, 20);
+            this.label2.Size = new System.Drawing.Size(967, 55);
             this.label2.TabIndex = 1;
             this.label2.Text = "Simulación Método Ordenamiento Burbuja";
             // 
             // txtNumero
             // 
-            this.txtNumero.Location = new System.Drawing.Point(147, 93);
+            this.txtNumero.Location = new System.Drawing.Point(466, 265);
+            this.txtNumero.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
             this.txtNumero.Name = "txtNumero";
-            this.txtNumero.Size = new System.Drawing.Size(81, 20);
+            this.txtNumero.Size = new System.Drawing.Size(248, 44);
             this.txtNumero.TabIndex = 2;
             this.txtNumero.TextChanged += new System.EventHandler(this.txtNumero_TextChanged);
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(299, 91);
+            this.btnAgregar.Location = new System.Drawing.Point(947, 259);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.Size = new System.Drawing.Size(238, 65);
             this.btnAgregar.TabIndex = 3;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
@@ -79,20 +83,23 @@
             // 
             // btnOrdenar
             // 
-            this.btnOrdenar.Location = new System.Drawing.Point(411, 91);
+            this.btnOrdenar.Location = new System.Drawing.Point(1302, 259);
+            this.btnOrdenar.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
             this.btnOrdenar.Name = "btnOrdenar";
-            this.btnOrdenar.Size = new System.Drawing.Size(75, 23);
+            this.btnOrdenar.Size = new System.Drawing.Size(238, 65);
             this.btnOrdenar.TabIndex = 4;
             this.btnOrdenar.Text = "Ordenar";
             this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
             // 
             // tabPage1
             // 
             this.tabPage1.AutoScroll = true;
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Location = new System.Drawing.Point(12, 58);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(715, 225);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.tabPage1.Size = new System.Drawing.Size(2266, 644);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Ver Simulación";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -101,24 +108,26 @@
             // tabPages
             // 
             this.tabPages.Controls.Add(this.tabPage1);
-            this.tabPages.Location = new System.Drawing.Point(44, 176);
+            this.tabPages.Location = new System.Drawing.Point(139, 501);
+            this.tabPages.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
             this.tabPages.Name = "tabPages";
             this.tabPages.SelectedIndex = 0;
-            this.tabPages.Size = new System.Drawing.Size(723, 251);
+            this.tabPages.Size = new System.Drawing.Size(2290, 714);
             this.tabPages.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
             this.tabPages.TabIndex = 5;
             // 
             // Burbuja
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(2533, 1281);
             this.Controls.Add(this.tabPages);
             this.Controls.Add(this.btnOrdenar);
             this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.txtNumero);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
             this.Name = "Burbuja";
             this.Text = "Método de ordenamiento BubbleSort";
             this.tabPages.ResumeLayout(false);
--- Form1.cs
+++ Form1.cs
@@ -5,8 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Ordenamiento
 {
@@ -58,7 +60,7 @@ namespace Ordenamiento
                 arreglo_botones[0].Width = 50;          //Definir ancho
                 arreglo_botones[0].Height = 50;         //Definir Alto
                 arreglo_botones[0].BackColor = Color.GreenYellow; //Definir color del botón
-                arreglo_botones[0].Text = dato  .ToString();
+                arreglo_botones[0].Text = dato.ToString();
                 Calcular_Longitud();
             }
             public Button[] Arreglo_Botones()
@@ -73,8 +75,10 @@ namespace Ordenamiento
             {
                 for (int j = 0; j < arreglo.Length - 1; j++)
                 {
+                   //Intercambio(ref Arreglo_Numeros, j + 1, j);
                     if (arreglo[i] == arreglo[j + 1])
                     {
+                        Intercambio(ref Arreglo_Numeros, j + 1, j);
                         int aux = arreglo[j];
                         arreglo[j] = arreglo[j + 1];
                         arreglo[j + 1] = aux;
@@ -107,7 +111,103 @@ namespace Ordenamiento
 
         private void tabPage1_Paint(object sender, PaintEventArgs e)
         {
+            if (estado)
+            {
+                Point xy = new Point(50, 70);   //método de librería Drawing
+                try
+                {
+                    Dibujar_Arreglo(ref Arreglo, xy, ref tabPage1);
+                    //Función que dibujará los datos en la simulación
+                }
+                catch
+                { }
+                estado = false;
+            }
+        }
+        public void Dibujar_Arreglo(ref Button[] Arreglo, Point xy, ref TabPage t)
+        {
+            for (int i = 1; i < Arreglo.Length; i++)
+            {
+                Arreglo[i].Location = xy;
+                t.Controls.Add(Arreglo[i]);
+                xy += new Size(70, 0);
+            }
+        }
 
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;   //Cambiamos la apariencia del cursor a modo espera
+
+            btnOrdenar.Enabled = false;
+            txtNumero.Enabled = false;
+            btnAgregar.Enabled = false;
+
+            BubbleSort(ref Arreglos_numeros, ref Arreglo);
+
+            this.Cursor = Cursors.Default;  //retorna la apariencia del cursor a uno por defecto
+
+            btnOrdenar.Enabled = true;
+            txtNumero.Enabled = true;
+            btnAgregar.Enabled = true;
+        }
+        public void Intercambio(ref Button[] boton, int a, int b)
+        {
+            string temp = boton[a].Text;    //Variable temporal
+
+            Point pa = boton[a].Location;   //definimos la posición inicial
+            Point pb = boton[b].Location;   //definimos posición final
+            int diferencia = pa.X - pb.X;   //distacia a moverse en forma horizontal
+            int x = 10;
+            int y = 10;
+            int t = 70;
+
+            while (y != 70)
+            {
+                Thread.Sleep(t);
+                /*Cantidad de movimientos a realizar cada botón en forma vertical
+                para agregar pausas al hilo de la ejecución */
+                boton[a].Location += new Size(0, 10);
+                boton[b].Location += new Size(0, -10);
+                y += 10;
+            }
+            while (x != diferencia + 10)     //Movimiento horizontal
+            {
+                Thread.Sleep(t);
+                boton[a].Location += new Size(-10, 0);
+                boton[b].Location -= new Size(10, 0);
+                x += 10;
+            }
+
+            y = 0;
+
+            while (y != -60)                //Movimiento vertical
+            {
+                Thread.Sleep(t);
+                boton[a].Location += new Size(0, -10);
+                boton[b].Location += new Size(0, +10);
+                y -= 10;
+            }
+
+            boton[a].Text = boton[b].Text;
+            boton[b].Text = temp;
+            boton[b].Location = pb;
+            boton[a].Location = pa;
+            estado = true;
+            tabPage1.Refresh();
+        }
+        static void Ordenamiento_Insercion (int[] array)
+        {
+            for ( int i = 0; i < array.Length; i++ )
+            {
+                int temp = array[i];
+                int j = i -1;
+                while ((j >= 0) && (array[j] > temp))
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = temp;
+            }
         }
     }
 }
